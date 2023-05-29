610e837f481f01bc21254da87555c29630948e54
Adeel Munir <111064925+Adeelmunir707@users.noreply.github.com>
2023-05-29 22:04:15 +0500

    Add files via upload

---
+++ programming with Javscript/week 1/week 1/week 1/switches_with_loops.js
@@ -0,0 +1,18 @@
+for (var i = 1; i <= 10; i++) {
+    switch(i) {
+        case 1:
+            console.log("Gold medal")
+            break
+        case 2:
+            console.log("Silver medal")
+            break
+        case 3:
+            console.log("Bronze medal")
+            break
+        default:
+            //this block will run if no condition matches
+            console.log(i)
+    }
+}
+console.log("\n")
+//===================================================
