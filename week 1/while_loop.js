1a2ba3da63c28951f5ca673f02c694ab3d6b2ec8
Adeel Munir <111064925+Adeelmunir707@users.noreply.github.com>
2023-05-29 22:49:48 +0500

    Add files via upload

---
+++ week 1/while_loop.js
@@ -0,0 +1,14 @@
+var i=1;
+while(i<8){
+    console.log("week day ",i,"of 7")
+    // i++
+    //i=i+1
+    i+=1
+}
+
+//==================================
+var year = 2018;
+while (year < 2023) {
+    console.log(year);
+    year++;
+};
