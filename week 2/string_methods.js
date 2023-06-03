87ebc4e0190473a883ca844ac91a7506e6f7269a
Adeel Munir <111064925+Adeelmunir707@users.noreply.github.com>
2023-06-03 20:14:26 +0500

    Add files via upload

---
+++ week 2/string_methods.js
@@ -0,0 +1,32 @@
+var greet = "Hello, ";
+var place = "World"
+
+greet.length; // 7
+greet.charAt(0); // 'H'
+"Wo".concat("rl").concat("d"); // 'World'
+
+"ho-ho-ho".indexOf('h'); // 0
+"ho-ho-ho".indexOf('o'); // 1
+"ho-ho-ho".indexOf('-'); // 2
+
+"ho-ho-ho".split("-"); // ['ho', 'ho', 'ho']
+
+greet.toUpperCase(); // "HELLO, "
+greet.toLowerCase(); // "hello, 
+
+
+  // Methods:
+// ===========
+// charAt() 
+
+// concat() 
+
+// indexOf() 
+
+// lastIndexOf() 
+
+// split() 
+
+// toUpperCase() 
+
+// toLowerCase()
